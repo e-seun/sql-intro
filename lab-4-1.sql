e1997af07d6716e2dbebac53b41fab8f78fbbaca
e-seun <seun.eniolorunda@kellogg.northwestern.edu>
2022-04-08 21:51:31 +0000

    solved

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,11 @@
 -- Expected result:
 -- 2935
 
+SELECT SUM(hits)
+FROM stats INNER JOIN players ON players.id = stats.player_id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
 
+SELECT * 
+FROM stats 
+WHERE player_id  = 1678
+LIMIT 5;
